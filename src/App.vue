7e9c6d6d8c3cb0296f42a966490a0d7cc32bd02d
jhaddrill <james.haddrill@visitscotland.com>
2022-04-28 12:06:22 +0100

    changae back

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="A change has happened again and again"/>
+    <HelloWorld msg="A change has happened again"/>
   </div>
 </template>
 
