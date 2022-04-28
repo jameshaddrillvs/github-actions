e75a94a42a53d868100b90dd4337c8b662c0c4e8
jhaddrill <james.haddrill@visitscotland.com>
2022-04-28 12:04:23 +0100

    intial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="A change has happened again"/>
+    <HelloWorld msg="A change has happened again and again"/>
   </div>
 </template>
 
